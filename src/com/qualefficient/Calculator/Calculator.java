a1a37e8e8ff69b470fbf35168ab73c15ba3dce0b
rahi <rahmanr@gmail.com>
2015-01-08 23:00:21 +0000

    add files to repo

---
+++ src/com/qualefficient/Calculator/Calculator.java
@@ -0,0 +1,21 @@
+package com.qualefficient.Calculator;
+
+/**
+ * Created by Rahi on 07/01/2015.
+ */
+public class Calculator {
+    public Calculator()
+    {
+    }
+
+    public int Add(int A, int B)
+    {
+        return A + B;
+    }
+
+    public int Subtract(int A, int B)
+    {
+        return A - B;
+    }
+
+}
